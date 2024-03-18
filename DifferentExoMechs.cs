525ef401ac41a4e69d2a1a4a5d49f0b8d1001b85
Dominic <farfetchedking541@gmail.com>
2024-03-18 12:06:35 -0500

    Reminder for later

--- DifferentExoMechs.cs
+++ DifferentExoMechs.cs
@@ -4,6 +4,7 @@ namespace DifferentExoMechs
 {
     public class DifferentExoMechs : Mod
     {
+        // Reminder for self: Don't create an Instance property in this class, use ModContent.GetInstance.
         public override void Load()
         {
             Luminance.Luminance.InitializeMod(this);
