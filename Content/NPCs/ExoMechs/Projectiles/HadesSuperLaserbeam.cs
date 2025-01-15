561e2823b72d2201dfe34d927254435f2b441091
Dominic Karma <farfetchedking541@gmail.com>
2025-01-15 15:06:00 -0600

    Nerf Hades laser self-damage

--- Content/NPCs/ExoMechs/Projectiles/HadesSuperLaserbeam.cs
+++ Content/NPCs/ExoMechs/Projectiles/HadesSuperLaserbeam.cs
@@ -242,7 +242,7 @@ namespace WoTM.Content.NPCs.ExoMechs.Projectiles
             return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), left, right, 16f, ref _);
         }
 
-        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers) => modifiers.FinalDamage += 240f;
+        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers) => modifiers.FinalDamage += 124f;
 
         public override bool ShouldUpdatePosition() => false;
     }
